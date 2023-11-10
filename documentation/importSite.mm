545c77be6d4af5f66602462e64e898e2df0f2b43
Laurence Delforge <laurencedelforge@hotmail.com>
2023-11-10 16:28:32 +0100

    first

---
+++ documentation/importSite.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1694527521336" ID="ID_1911226759" MODIFIED="1694527521336" TEXT="New Mindmap"/>
+</map>
---
+++ documentation/map_site.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ffffff" COLOR="#3333ff" CREATED="1694522688831" ID="ID_404098936" MODIFIED="1694527191890">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Home
+    </p>
+    <p style="text-align: center">
+      <font size="2">index.html</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#009933"/>
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1694525404363" ID="ID_642813704" MODIFIED="1694525544567" POSITION="right" TEXT="About - about.html"/>
+<node CREATED="1694525584229" ID="ID_1067208957" MODIFIED="1694525602492" POSITION="right" TEXT="Etudes - etudes.html"/>
+<node CREATED="1694525703900" ID="ID_115674845" MODIFIED="1694525725535" POSITION="right" TEXT="Webdev - webdev.html"/>
+<node CREATED="1694525637313" ID="ID_1304556686" MODIFIED="1694526165964" POSITION="right" TEXT="Portfolio - portfolio.html">
+<node COLOR="#ff66ff" CREATED="1694525985551" ID="ID_1923545427" MODIFIED="1694526545671" TEXT="grafic design"/>
+<node COLOR="#ff66ff" CREATED="1694525998842" ID="ID_808655881" LINK="https://github.com/Loknit9/" MODIFIED="1694526654490" TEXT="webdev"/>
+</node>
+<node CREATED="1694525730326" ID="ID_1933534703" MODIFIED="1694526168655" POSITION="right" TEXT="Jobs - jobs.html"/>
+<node COLOR="#009933" CREATED="1694525557738" ID="ID_693636176" LINK="mailto:l.delforge@hotmail.com" MODIFIED="1694526923140" POSITION="right" TEXT="Contact - contact.html">
+<node CREATED="1694525783136" ID="ID_1155106476" LINK="../../../Users/l.delforge/OneDrive%20-%20Interface3/Desktop/Laurence%20Delforge%20Examen%20r&#xe9;seau.pdf" MODIFIED="1694526406656" TEXT="cv.pdf"/>
+</node>
+</node>
+</map>
