7b866a395f03aa3bfef9514f6157e86331f653c1
Laurence Delforge <laurencedelforge@hotmail.com>
2023-11-14 09:13:25 +0100

    respons forme+header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,7 +8,7 @@ const bottom_btn = document.getElementById("bleu");
 const movable = document.querySelector(".movable");
 
 center_btn.addEventListener("click", function(){
-    movable.style.transform="translate=(0vw, 0vh)";
+    movable.style.transform="";
     console.log(movable.style.transform);
 });
 
